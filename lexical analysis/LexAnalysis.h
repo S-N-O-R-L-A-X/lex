6de9a5cc34b72fcbb155caebfd000cfe2675b29f
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-04-28 22:54:15 +0800

    pass 2

--- lexical analysis/LexAnalysis.h
+++ lexical analysis/LexAnalysis.h
@@ -14,7 +14,9 @@ using namespace std;
 /* 不要修改这个标准输入函数 */
 unordered_map<string,int> string2num;
 unordered_set<char> sign;
-vector<string> ans;
+vector<string> ans,comment;
+vector<int> cntBlanks;
+
 void init(){
 	string2num={{"auto",1},{"break",2},{"case",3},{"char",4},{"const",5},{"continue",6},{"default",7},{"do",8},{"double",9},{"else",10},{"enum",11},{"extern",12},{"float",13},{"for",14},{"goto",15},{"if",16},{"int",17},{"long",18},{"register",19},{"return",20},{"short",21},{"signed",22},{"sizeof",23},{"static",24},{"struct",25},{"switch",26},{"typedef",27},{"union",28},{"unsigned",29},{"void",30},{"volatile",31},{"while",32},{"-",33},{"--",34},{"-=",35},{"->",36},{"!",37},{"!=",38},{"%",39},{"%=",40},{"&",41},{"&&",42},{"&=",43},{"(",44},{")",45},{"*",46},{"*=",47},{",",48},{".",49},{"/",50},{"/=",51},{":",52},{";",53},{"?",54},{"[",55},{"]",56},{"^",57},{"^=",58},{"{",59},{"|",60},{"||",61},{"|=",62},{"}",63},{"~",64},{"+",65},{"++",66},{"+=",67},{"<",68},{"<<",69},{"<<=",70},{"<=",71},{"=",72},{"==",73},{">",74},{">=",75},{">>",76},{">>=",77},{"\"",78},{"/*注释*/",79},{"常数",80},{"标识符",81}};
 	sign={'<', '>', '=', '+', '-', '*', '/', ':', ';', '(', ')', '[', ']', '|', '"', ',', '&'};
@@ -30,26 +32,67 @@ void read_prog(string& prog)
 /* 你可以添加其他函数 */
 void easy_deal(const string &str,int cnt){
 	if(string2num.find(str)!=string2num.end()){
-		ans.push_back(to_string(cnt)+":<"+str+","+to_string(string2num[str])+">");
+		ans.push_back(to_string(cnt)+": <"+str+","+to_string(string2num[str])+">");
 	}
 	else{
 		if(str>="0"&&str<="99999999999"){
-			ans.push_back(to_string(cnt)+":<"+str+",80"+">");
+			ans.push_back(to_string(cnt)+": <"+str+",80"+">");
 		}
 		else{
-			ans.push_back(to_string(cnt)+":<"+str+",81"+">");
+			ans.push_back(to_string(cnt)+": <"+str+",81"+">");
 		}
 		
 	}
 }
 
+void deal_inline_comment(string &str){
+	string tmp=str;
+	while(tmp.find("//")!=tmp.npos){
+		tmp=tmp.substr(tmp.find("//"));
+		int stop=0,cnt=0;
+		for(int i=0;i<tmp.size();++i){
+			if(tmp[i]=='\n'){
+				stop=i;
+				break;
+			}
+			else if(tmp[i]==' '){
+				cnt++;
+			}
+		}
+		comment.push_back(tmp.substr(0,stop));
+		cntBlanks.push_back(cnt);
+		tmp=tmp.substr(stop);
+	}
+}
+
+void predeal(string &str){ //deal with comments
+	for(int i=0;i<str.size();++i){
+		if(str[i]==';'){
+			str=str.substr(0,i+1)+' '+str.substr(i+1);
+		}
+	}
+	deal_inline_comment(str);
+	// void deal_block_comment();
+}
+
 void deal(string &str){
+	predeal(str);
 	stringstream ss(str);
 	string now;
-	int cnt=1;
+	int cnt=1,skip=0,commentPointer=0;
 	while(ss>>now){
+		if(skip>0){
+			skip--;
+			continue;
+		}
+		if(now[0]=='/'&&now[1]=='/'){
+			skip=cntBlanks[commentPointer];
+			ans.push_back(to_string(cnt++)+": <"+comment[commentPointer++]+",79"+">");
+			continue;
+		}
+
 		if(string2num.find(now)!=string2num.end()){
-			ans.push_back(to_string(cnt)+":<"+now+","+to_string(string2num[now])+">");
+			ans.push_back(to_string(cnt)+": <"+now+","+to_string(string2num[now])+">");
 			cnt++;
 		}
 		else{
@@ -57,12 +100,22 @@ void deal(string &str){
 			for(int i=0;i<now.size();){
 				if(sign.find(now[i])!=sign.end()){//sign
 					string former=now.substr(start,i-start);
-					easy_deal(former,cnt++);
+					if(former!=""){
+						easy_deal(former,cnt++);
+					}
+					
 					start=i;
 					while(i<now.size()&&sign.find(now[i])!=sign.end()){//always sign
 						i++;
 					}
 					string signs=now.substr(start,i-start);
+
+					// if(signs.size()>=2&&signs[0]=='/'&&signs[1]=='/'){
+					// 	skip=cntBlanks[commentPointer];
+					// 	ans.push_back(to_string(cnt)+": <"+comment[commentPointer++]+",79"+">");
+					// 	break;
+					// }
+
 					if(string2num.find(signs)!=string2num.end()){
 						easy_deal(signs,cnt++);
 					}
