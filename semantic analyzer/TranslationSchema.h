35abfa1c3541fd6bea42f1e19e8f96d9633c5d4e
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2022-06-16 23:36:46 +0800

    start

--- semantic analyzer/TranslationSchema.h
+++ semantic analyzer/TranslationSchema.h
@@ -7,8 +7,93 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <stack>
+#define endl "\n"<<flush
 using namespace std;
-/* 不要修改这个标准输入函数 */
+
+unordered_map<string,int> TERMINALS,NONTERMINALS,identifier2idx;
+vector<vector<pair<int,string>>> actionTable; //shift : the state reduce: pop,add
+vector<vector<int>> gotoTable;
+stack<pair<string,int>> stk;
+vector<string> words,ans,oldString,newString;
+
+struct word {
+    int type;
+    double value;
+};
+
+vector<word> identifiers;
+
+void split(string &prog){
+	string tmp=prog;
+	int pre=0,cur=tmp.find("\n");
+	while(cur!=tmp.npos){
+		if(cur>pre){
+			words.push_back(tmp.substr(pre,cur-pre));
+		}
+		pre=cur+1;
+		cur=tmp.find("\n",pre);
+	}
+	if(pre!=tmp.size()){
+		words.push_back(tmp.substr(pre));
+	}
+}
+
+void init(string &prog){
+    NONTERMINALS={{"program",0},{"stmt",1},{"compoundstmt",2},{"stmts",3},{"ifstmt",4},{"whilestmt",5},{"assgstmt",6},{"boolexpr",7},{"boolop",8},{"arithexpr",9},{"arithexprprime",10},{"multexpr",11},{"multexprprime",12},{"simpleexpr",13}};
+	TERMINALS={{"{",0},{"}",1},{"while",2},{"if",3},{"ID",4},{"NUM",5},{";",6},{"*",7},{"/",8},{"+",9},{"-",10},{"(",11},{")",12},{">",13},{"<",14},{"==",15},{"<=",16},{">=",17},{"=",18},{"then",19},{"else",20},{"$",21}};
+	split(prog);
+	actionTable.resize(100,vector<pair<int,string>>(22));
+    gotoTable.resize(100,vector<int>(14));
+	// create_table();
+	stk.push({"start",0});
+}
+
+void error(){
+
+}
+
+void readParameters(){
+    stringstream ss(words[0]);
+    string now;
+    int cnt=0;
+
+    while(ss){
+        vector<string> v(5);
+        for(int j=0;j<5;++j){ //read 5 words 
+            ss>>v[j];
+        }
+        word tmp;
+        identifier2idx[v[1]]=cnt++;
+        if(v[0]=="int"){
+            tmp.type=0;
+            if(v[3].find('.')!=v[3].npos){//int
+                error();
+                return ;
+            }
+            else{
+                tmp.value=stoi(v[3]);
+            }
+        }
+        else if(v[0]=="real"){
+            tmp.type=1;
+            tmp.value=stod(v[3]);
+        }
+        else{
+            error();
+            return ;
+        }
+    }
+	
+}
+
+void semanticAnalyze(){
+    
+}
+
 void read_prog(string& prog)
 {
     char c;
